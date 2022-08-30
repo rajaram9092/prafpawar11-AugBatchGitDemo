02f5b77ebf6dd13a719fe3975ce167785e3f7cbf
prafpawar11 <prafpawar11@gmail.com>
2022-08-30 20:03:49 +0530

    title test cases

--- src/HomePage/HomepageDemo1.java
+++ src/HomePage/HomepageDemo1.java
@@ -3,9 +3,7 @@ package HomePage;
 public class HomepageDemo1 {
 
 	public static void main(String[] args)
-
 	{
-
 		System.out.println("Verify the title test cases");
 
 	}
---
+++ src/HomePage/Sample1.java
@@ -0,0 +1,11 @@
+package HomePage;
+
+public class Sample1 {
+
+	public static void main(String[] args)
+	{
+	System.out.println("Verify the home page url");
+
+	}
+
+}
